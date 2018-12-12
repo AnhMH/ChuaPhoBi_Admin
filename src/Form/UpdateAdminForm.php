c1062bc9ce9c7a0963c13e8e5abaef67c3f3d45a
AnhMH <anhmh@lifull-tech.vn>
2018-12-12 21:48:08 +0700

    Modify update profile

--- src/Form/UpdateAdminForm.php
+++ src/Form/UpdateAdminForm.php
@@ -15,12 +15,6 @@ class UpdateAdminForm extends Form {
      */
     protected function _buildValidator(Validator $validator) {
         return $validator
-                        ->notEmpty('account', __('MESSAGE_REQUIRED_LOGIN_ID'))
-                        ->notEmpty('email', __('MESSAGE_REQUIRED_EMAIL'))
-                        ->add('email', 'validFormat', [
-                            'rule' => 'email',
-                            'message' => '"MESSAGE_INVALID_EMAIL_FORMAT'
-                        ])
                         ->notEmpty('name', __('MESSAGE_REQUIRED_NAME'))
         ;
     }
